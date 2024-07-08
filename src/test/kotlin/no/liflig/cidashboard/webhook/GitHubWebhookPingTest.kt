f53c31b17216815791a3911939be8bc83019ea91
Kristian Rekstad <kre@capraconsulting.no>
2024-07-08 13:28:53 +0200

    use loadResource for reading data

--- src/test/kotlin/no/liflig/cidashboard/webhook/GitHubWebhookPingTest.kt
+++ src/test/kotlin/no/liflig/cidashboard/webhook/GitHubWebhookPingTest.kt
@@ -2,13 +2,14 @@ package no.liflig.cidashboard.webhook
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import test.util.loadResource
 
 class GitHubWebhookPingTest {
 
   @Test
   fun `should deserialize from json`() {
     // Given
-    val pingEventJson = javaClass.classLoader.getResource("acceptancetests/webhook/github-ping-body.json")!!.readText()
+    val pingEventJson = loadResource("acceptancetests/webhook/github-ping-body.json")
 
     // When
     val event = GitHubWebhookPing.fromJson(pingEventJson)
