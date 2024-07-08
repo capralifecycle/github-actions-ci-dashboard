553d499271c76ddb474c8a247d759667b4787672
Kristian Rekstad <kre@capraconsulting.no>
2024-07-08 13:31:00 +0200

    Send events to IncomingWebhookService
    CALS-821

--- src/main/kotlin/no/liflig/cidashboard/webhook/WebhookEndpoint.kt
+++ src/main/kotlin/no/liflig/cidashboard/webhook/WebhookEndpoint.kt
@@ -10,7 +10,9 @@ import org.http4k.lens.Header
 import org.http4k.lens.HeaderLens
 import org.http4k.lens.nonBlankString
 
-class WebhookEndpoint : HttpHandler {
+class WebhookEndpoint(
+    private val incomingWebhookService: IncomingWebhookService = IncomingWebhookService()
+) : HttpHandler {
   companion object {
     private val log = KotlinLogging.logger {}
 
@@ -22,27 +24,39 @@ class WebhookEndpoint : HttpHandler {
   override fun invoke(request: Request): Response {
     val eventType: String = webhookEventType(request)
 
+    // TODO: stop logging body and headers when we have enough sample data.
     withLoggingContext(
         "request.body" to request.bodyString(),
         "request.headers" to request.headers.joinToString()) {
           log.info { "Got webhook payload for $eventType" }
         }
 
-    return Response(Status.OK)
-
-    /*
     try {
       when (eventType) {
-        "ping" -> GitHubWebhookPing.bodyLens(request)
-        "workflow_run" -> null
-        else -> log.warn { "Unrecognized event: $eventType" }
+        "ping" -> {
+          val event = GitHubWebhookPing.bodyLens(request)
+          incomingWebhookService.handlePing(event)
+        }
+        "workflow_run" -> {
+          val event = GitHubWebhookWorkflowRun.bodyLens(request)
+          incomingWebhookService.handleWorkflowRun(event)
+        }
+        else -> {
+          withLoggingContext(
+              "request.body" to request.bodyString(),
+              "request.headers" to request.headers.joinToString()) {
+                log.error { "Unrecognized event: $eventType" }
+              }
+        }
       }
-      // Todo handle payload
-    } catch (ex: Throwable) {
-      log.error(ex) { "Failed to extract webhook payload for workflow_run" }
-    }*/
 
+      return Response(Status.OK)
+    } catch (ex: Throwable) {
+      log.error(ex) { "Failed to extract webhook payload for $eventType" }
+      return Response(Status.INTERNAL_SERVER_ERROR)
+    }
   }
 }
 
+/** Marker interface to easily find all payload classes. */
 interface WebhookPayload
