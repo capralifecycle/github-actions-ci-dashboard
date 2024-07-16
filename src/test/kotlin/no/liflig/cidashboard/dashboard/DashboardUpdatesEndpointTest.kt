780c4cc753693161239855e062fae70939b591b2
Kristian Rekstad <kre@capraconsulting.no>
2024-07-16 17:34:17 +0200

    Update snapshot test of updates

--- src/test/kotlin/no/liflig/cidashboard/dashboard/DashboardUpdatesEndpointTest.kt
+++ src/test/kotlin/no/liflig/cidashboard/dashboard/DashboardUpdatesEndpointTest.kt
@@ -5,6 +5,7 @@ import io.mockk.mockk
 import no.liflig.cidashboard.common.http4k.MissingHelper
 import no.liflig.cidashboard.persistence.CiStatus
 import no.liflig.cidashboard.persistence.createCiStatus
+import no.liflig.snapshot.verifyStringSnapshot
 import org.assertj.core.api.Assertions.assertThat
 import org.http4k.core.Method
 import org.http4k.core.Request
@@ -14,7 +15,7 @@ import org.junit.jupiter.api.Test
 class DashboardUpdatesEndpointTest {
 
   @Test
-  fun `should render empty dashboard list to say no builds`() {
+  fun `should render empty list to say no builds`() {
     // Given
     val ciStatuses: List<CiStatus> = emptyList()
     val dashboardId = "1"
@@ -37,7 +38,7 @@ class DashboardUpdatesEndpointTest {
   }
 
   @Test
-  fun `should render all dashboards from the service to html`() {
+  fun `should render all ci statuses from the service to html`() {
     // Given
     val ciStatuses: List<CiStatus> =
         listOf(
@@ -68,49 +69,7 @@ class DashboardUpdatesEndpointTest {
         .`as`("Invalid template variables. Make sure any inline class is not mangled in CiStatus")
         .doesNotContain(MissingHelper.ERROR_PREFIX)
         .`as`("Invalid rendered output")
-        .isEqualTo(
-            """  <div class="status status--SUCCEEDED" id="1">
-    <span class="status__repo-name">repo-a</span>
-    <span class="status__repo-branch">master</span>
-    <span class="status__last-updated">2024-07-05T12:25:40Z</span>
-    <span class="status__triggered-by">krissrex</span>
-    <div class="commit status__commit">
-      <span class="commit__title">Add feature</span>
-      <span class="commit__sha">123abc</span>
-      <div class="commiter commit__commiter">
-        <img src="https://avatars.githubusercontent.com/u/7364831?v&#x3D;4" alt="Avatar" class="commiter__avatar">
-        <span class="commiter__username">krissrex</span>
-      </div>
-    </div>
-  </div>
-  <div class="status status--IN_PROGRESS" id="2">
-    <span class="status__repo-name">repo-b</span>
-    <span class="status__repo-branch">master</span>
-    <span class="status__last-updated">2024-07-05T12:25:40Z</span>
-    <span class="status__triggered-by">krissrex</span>
-    <div class="commit status__commit">
-      <span class="commit__title">Add feature</span>
-      <span class="commit__sha">123abc</span>
-      <div class="commiter commit__commiter">
-        <img src="https://avatars.githubusercontent.com/u/7364831?v&#x3D;4" alt="Avatar" class="commiter__avatar">
-        <span class="commiter__username">krissrex</span>
-      </div>
-    </div>
-  </div>
-  <div class="status status--FAILED" id="3">
-    <span class="status__repo-name">repo-c</span>
-    <span class="status__repo-branch">master</span>
-    <span class="status__last-updated">2024-07-05T12:25:40Z</span>
-    <span class="status__triggered-by">krissrex</span>
-    <div class="commit status__commit">
-      <span class="commit__title">Add feature</span>
-      <span class="commit__sha">123abc</span>
-      <div class="commiter commit__commiter">
-        <img src="https://avatars.githubusercontent.com/u/7364831?v&#x3D;4" alt="Avatar" class="commiter__avatar">
-        <span class="commiter__username">krissrex</span>
-      </div>
-    </div>
-  </div>
-""")
+
+    verifyStringSnapshot("dashboard-updates-endpoint/updates.html", actualHtmlBody)
   }
 }
