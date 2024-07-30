a6ac2efc347c2a0a8ef3fbf1754eaa6df2e0475f
mikaelthd <mth@capraconsulting.no>
2024-07-30 12:09:00 +0200

    Add config html rendering test
    CALS-825

--- src/test/kotlin/no/liflig/cidashboard/dashboard/DashboardUpdatesEndpointTest.kt
+++ src/test/kotlin/no/liflig/cidashboard/dashboard/DashboardUpdatesEndpointTest.kt
@@ -2,6 +2,7 @@ package no.liflig.cidashboard.dashboard
 
 import io.mockk.every
 import io.mockk.mockk
+import no.liflig.cidashboard.DashboardConfig
 import no.liflig.cidashboard.DashboardConfigId
 import no.liflig.cidashboard.common.config.ClientSecretToken
 import no.liflig.cidashboard.persistence.CiStatus
@@ -64,7 +65,7 @@ class DashboardUpdatesEndpointTest {
                     createCiStatus(
                         id = "3", repoName = "repo-c", lastStatus = CiStatus.PipelineStatus.FAILED),
                 ))
-    val dashboardId = DashboardConfigId("2")
+    val dashboardId = null
     val updatesService: DashboardUpdatesService =
         mockk() { every { getUpdatedDashboardData(dashboardId) } returns ciStatuses }
 
@@ -89,5 +90,57 @@ class DashboardUpdatesEndpointTest {
         .`as`("Invalid rendered output")
 
     verifyStringSnapshot("dashboard-updates-endpoint/updates.html", actualHtmlBody)
+  }
+
+  @Test
+  fun `should render all ci statuses form service to html with config`() {
+    // Given
+    val dashboardId = DashboardConfigId("custom-config")
+    val ciStatuses =
+        DashboardData(
+            config = DashboardConfig(dashboardId),
+            lastBuilds =
+                listOf(
+                    createCiStatus(
+                        id = "1",
+                        repoName = "repo-a",
+                        lastStatus = CiStatus.PipelineStatus.SUCCEEDED),
+                    createCiStatus(
+                        id = "2",
+                        repoName = "repo-b",
+                        lastStatus = CiStatus.PipelineStatus.IN_PROGRESS),
+                    createCiStatus(
+                        id = "3", repoName = "repo-c", lastStatus = CiStatus.PipelineStatus.FAILED),
+                ),
+            allFailedBuilds =
+                listOf(
+                    createCiStatus(
+                        id = "3", repoName = "repo-c", lastStatus = CiStatus.PipelineStatus.FAILED),
+                ))
+
+    val updatesService: DashboardUpdatesService =
+        mockk() { every { getUpdatedDashboardData(dashboardId) } returns ciStatuses }
+
+    val secretToken = "my-secret-token"
+    val endpoint =
+        DashboardUpdatesEndpoint(
+            updatesService, secretToken = ClientSecretToken(secretToken), useHotReload = false)
+
+    val request =
+        Request(Method.GET, "")
+            .with(DashboardConfigId.queryLens of dashboardId)
+            .with(DashboardUpdatesEndpoint.tokenLens of secretToken)
+
+    // When
+    val response = endpoint(request)
+    val actualHtmlBody = response.bodyString()
+
+    // Then
+    assertThat(actualHtmlBody)
+        .`as`("Invalid template variables. Make sure any inline class is not mangled in CiStatus")
+        .doesNotContain(MissingHelper.ERROR_PREFIX)
+        .`as`("Invalid rendered output")
+
+    verifyStringSnapshot("dashboard-updates-endpoint/updates-with-config.html", actualHtmlBody)
   }
 }
