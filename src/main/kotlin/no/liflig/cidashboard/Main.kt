0ed1a3031444fbbece1334016b1fa8532860d873
hermannm <hermann.morkrid@gmail.com>
2025-11-26 13:34:11 +0100

    Improve startup error logging (align with REST service baseline)

--- src/main/kotlin/no/liflig/cidashboard/Main.kt
+++ src/main/kotlin/no/liflig/cidashboard/Main.kt
@@ -1,11 +1,29 @@
 package no.liflig.cidashboard
 
+import kotlin.system.exitProcess
 import no.liflig.cidashboard.common.config.Config
+import no.liflig.logging.getLogger
 import org.slf4j.bridge.SLF4JBridgeHandler
 
-fun main(args: Array<String>) {
-  SLF4JBridgeHandler.removeHandlersForRootLogger()
-  SLF4JBridgeHandler.install()
+private val log = getLogger()
 
-  App(Config.load()).start()
+fun main(args: Array<String>) {
+  /** Ensures that all uncaught exceptions are logged. */
+  Thread.setDefaultUncaughtExceptionHandler { _, e: Throwable ->
+    log.error(e) { "Uncaught exception in thread" }
+  }
+
+  try {
+    SLF4JBridgeHandler.removeHandlersForRootLogger()
+    SLF4JBridgeHandler.install()
+
+    App(Config.load()).start()
+  } catch (e: Throwable) {
+    /**
+     * If we failed to load config/start the application, we want to log it, to make sure that we
+     * don't lose the exception.
+     */
+    log.error(e) { "Application startup failed" }
+    exitProcess(status = 1)
+  }
 }
